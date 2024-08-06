0cfaf5436ec13b48bcaae96e0858bad26061c8a3
ATA TOURE <157533576+ATA250323@users.noreply.github.com>
2024-08-06 08:10:58 +0100

    premiere commentaire

---
+++ resources/views/layouts/partifixe/footerar.blade.php
@@ -0,0 +1,199 @@
+ <!-- Footer Start -->
+ <div class="container-fluid footer py-3 wow fadeIn" data-wow-delay="0.2s">
+     <div class="container py-3">
+         <div class="row g-5">
+             <div class="col-xl-12">
+                 <div class="mb-5">
+                     <div class="row g-4">
+                         <div class="col-md-6 col-lg-6 col-xl-4">
+                             <center>
+                                 <div class="footer-item">
+                                     <h4 class="text-white mb-4">رقم التسجيل</h4>
+                                     <p class="text-white mb-3">2014 le N°6/064/P-SG-STCCD-DCDI-ASS du 11 Décembre 2014
+                                     </p>
+                                     <div class="position-relative rounded-pill mb-4">
+                                         <input class="form-control rounded-pill w-100 py-3 ps-4 pe-5" type="text"
+                                             placeholder="أدخل بريدك الإلكتروني">
+                                         <button type="button"
+                                             class="btn btn-primary rounded-pill position-absolute top-0 end-0 py-2 mt-2 me-2">سجل</button>
+                                     </div>
+                                     <div class="d-flex flex-shrink-0">
+                                         <div class="footer-btn">
+                                             <a href="+229 97 95 90 43"
+                                                 class="btn btn-lg-square rounded-circle position-relative wow tada"
+                                                 data-wow-delay=".9s">
+                                                 <i class="fa fa-phone-alt fa-2x"></i>
+                                                 <div class="position-absolute" style="top: 2px; right: 12px;">
+                                                     <span><i class="fa fa-comment-dots text-secondary"></i></span>
+                                                 </div>
+                                             </a>
+                                         </div>
+                                         <div class="d-flex flex-column ms-3 flex-shrink-0">
+                                             <span>اتصل بخبراءنا</span>
+                                             <a href="+229 97 95 90 43"><span class="text-white">+229 97 95 90
+                                                     43</span></a>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </center>
+                         </div>
+                         <div class="col-md-6 col-lg-6 col-xl-4">
+                             <center>
+                                 <div class="footer-item">
+                                     <h4 class="mb-4 text-white">الصور</h4>
+                                     <div class="row g-3">
+                                         <div class="col-4">
+                                             <div class="footer-instagram rounded">
+                                                 <img src="{{ asset('assets/img/2015/IMG-20151227-WA0132.jpg') }}"
+                                                     class="img-fluid w-100" alt="">
+                                                 <div class="footer-search-icon">
+                                                     <a href="{{ asset('assets/img/2015/IMG-20151227-WA0132.jpg') }}"
+                                                         data-lightbox="footerInstagram-1" class="my-auto"><i
+                                                             class="fas fa-link text-white"></i></a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                         <div class="col-4">
+                                             <div class="footer-instagram rounded">
+                                                 <img src="{{ asset('assets/img/2016/IMG_20160610_190753.jpg') }}"
+                                                     class="img-fluid w-100" alt="">
+                                                 <div class="footer-search-icon">
+                                                     <a href="{{ asset('assets/img/2016/IMG_20160610_190753.jpg') }}"
+                                                         data-lightbox="footerInstagram-2" class="my-auto"><i
+                                                             class="fas fa-link text-white"></i></a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                         <div class="col-4">
+                                             <div class="footer-instagram rounded">
+                                                 <img src="{{ asset('assets/img/2016/IMG_20160702_135635.jpg') }}"
+                                                     class="img-fluid w-100" alt="">
+                                                 <div class="footer-search-icon">
+                                                     <a href="{{ asset('assets/img/2016/IMG_20160702_135635.jpg') }}"
+                                                         data-lightbox="footerInstagram-3" class="my-auto"><i
+                                                             class="fas fa-link text-white"></i></a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                         <div class="col-4">
+                                             <div class="footer-instagram rounded">
+                                                 <img src="{{ asset('assets/img/2022/1universite322.jpg') }}"
+                                                     class="img-fluid w-100" alt="">
+                                                 <div class="footer-search-icon">
+                                                     <a href="{{ asset('assets/img/2022/1universite322.jpg') }}"
+                                                         data-lightbox="footerInstagram-4" class="my-auto"><i
+                                                             class="fas fa-link text-white"></i></a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                         <div class="col-4">
+                                             <div class="footer-instagram rounded">
+                                                 <img src="{{ asset('assets/img/2023/٢٠٢٣١٢١٥_١٠١٦١٥.jpg') }}"
+                                                     class="img-fluid w-100" alt="">
+                                                 <div class="footer-search-icon">
+                                                     <a href="{{ asset('assets/img/2023/٢٠٢٣١٢١٥_١٠١٦١٥.jpg') }}"
+                                                         data-lightbox="footerInstagram-5" class="my-auto"><i
+                                                             class="fas fa-link text-white"></i></a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                         <div class="col-4">
+                                             <div class="footer-instagram rounded">
+                                                 <img src="{{ asset('assets/img/2024/موزة.jpg') }}"
+                                                     class="img-fluid w-100" alt="">
+                                                 <div class="footer-search-icon">
+                                                     <a href="{{ asset('assets/img/2024/موزة.jpg') }}"
+                                                         data-lightbox="footerInstagram-6" class="my-auto"><i
+                                                             class="fas fa-link text-white"></i></a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </center>
+                         </div>
+                         <div class="col-md-6 col-lg-6 col-xl-4 ">
+                             <center>
+                                 <div class="footer-item">
+                                     <a href="index.html" class="p-0">
+                                         <h3 class="text-white">الوفاء<img src="{{ asset('assets/img/logo.jpg') }}"
+                                                 width="50" height="50" alt="">
+                                         </h3>
+                                         <!-- <img src="img/logo.png" alt="Logo"> -->
+                                     </a>
+                                     <p class="text-white mb-4">جمعية الوفاء لخدمة المجتجع</p>
+                                     <div class="footer-btn d-flex">
+                                         <a class="btn btn-md-square rounded-circle me-3" href="#"><i
+                                                 class="fab fa-facebook-f"></i></a>
+                                         <a class="btn btn-md-square rounded-circle me-3" href="#"><i
+                                                 class="fab fa-twitter"></i></a>
+                                         <a class="btn btn-md-square rounded-circle me-3" href="#"><i
+                                                 class="fab fa-instagram"></i></a>
+                                         <a class="btn btn-md-square rounded-circle me-0" href="#"><i
+                                                 class="fab fa-linkedin-in"></i></a>
+                                     </div>
+                                 </div>
+                             </center>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="pt-3" style="border-top: 1px solid rgba(255, 255, 255, 0.08);">
+                     <div class="row g-0">
+                         <div class="col-12">
+                             <div class="row g-4">
+                                 <div class="col-lg-6 col-xl-4">
+                                     <div class="d-flex">
+                                         <div class="btn-xl-square bg-primary text-white rounded p-4 me-4">
+                                             <i class="fas fa-map-marker-alt fa-2x"></i>
+                                         </div>
+                                         <div>
+                                             <h4 class="text-white">مقعد</h4>
+                                             <p class="mb-3">Route de Kolondé-Djougou - RN8</p>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="col-lg-6 col-xl-4">
+                                     <div class="d-flex">
+                                         <div class="btn-xl-square bg-primary text-white rounded p-4 me-4">
+                                             <i class="fas fa-envelope fa-2x"></i>
+                                         </div>
+                                         <div>
+                                             <h4 class="text-white">بريد إلكتروني</h4>
+                                             <p class="mb-0">info@example.com</p>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="col-lg-6 col-xl-4">
+                                     <div class="d-flex">
+                                         <div class="btn-xl-square bg-primary text-white rounded p-4 me-4">
+                                             <i class="fa fa-phone-alt fa-2x"></i>
+                                         </div>
+                                         <div>
+                                             <h4 class="text-white">هاتف</h4>
+                                             <p class="mb-0">(+229) 97 43 21 88 </p>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- Footer End -->
+
+ <!-- Copyright Start -->
+ <div class="container-fluid copyright py-2">
+     <div class="container">
+         <div class="row g-12 align-items-center">
+             <div class="col-md-12 text-center  mb-md-0">
+                 &copy; Copyright <strong><span>الوفاء</span></strong>جميع الحقوق محفوظة
+
+             </div>
+
+         </div>
+     </div>
+ </div>
+ <!-- Copyright End -->
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('ar/accueil');
 });
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
